71c61631810b7f8c27f613360dfebd8e3126247d
Tomáš Voslař <t.voslar@gmail.com>
2014-09-20 18:34:29 +0100

    Update Webcms.main.js

--- js/Webcms/Webcms.main.js
+++ js/Webcms/Webcms.main.js
@@ -26,6 +26,8 @@ Webcms.prototype = {
 		self.tour = new WebcmsTour(self);
 		self.basePath = $('base').attr('href');
 
+		self.registerDatePickers();
+		
 		/**
 		 * Nette ajax Grido extension.
 		 * @author Petr BugyĂ­k
@@ -48,6 +50,16 @@ Webcms.prototype = {
 							$('#myModal').modal('hide');
 						}
 					}
+
+					self.registerDatePickers();
+
+					$(document).off('change', '.daterange');
+					$(document).on('change', '.daterange', function(){
+						var splitted = $(this).val().split('-');
+						if (splitted.length > 2) {
+							$(this).val(splitted[0] + '-' + splitted[2]);
+						}
+					});
 
 					//scroll up after ajax update
 					$('html, body').animate({scrollTop: 0}, 400);
@@ -74,7 +86,7 @@ Webcms.prototype = {
 		});
 
 		// links context menu
-		/*$(document).on('contextmenu', 'a', function(e) {
+		$(document).on('contextmenu', 'a', function(e) {
 			e.preventDefault();
 
 			$('.context-menu').remove();
@@ -87,7 +99,7 @@ Webcms.prototype = {
 
 		$(document).on('click', function() {
 			$('.context-menu').remove();
-		});*/
+		});
 
 		//ajax loader animation
 		$(document).ajaxStart(function() {
@@ -121,6 +133,21 @@ Webcms.prototype = {
 		});
 
 		self.initTextEditors();
+	},
+	registerDatePickers: function() {
+		$('.date').datepicker({
+			format: "m.d.yyyy",
+			autoclose: true,
+			language: "cs"
+		});
+
+		$('.daterange').datepicker({
+			format: "m.d.yyyy",
+			multidate: true,
+			multidateSeparator: "-",
+			autoclose: true,
+			language: "cs"
+		});
 	},
 	registerExternal: function(ext) {
 		this.externals.push(ext);
